e3705fc1ca70074d92bd07874ce32deec27f24ce
Jesse Brandenburg <jesselbrandenburg@gmail.com>
2013-11-15 11:23:16 -0600

    Commiting before merging with Mike's code

--- library.rb
+++ library.rb
@@ -27,20 +27,33 @@ class Library
   end
 
   def check_out(user, book)
+  if user.borrowed_books.length == 2
+    return "You already have 2 books checked-out, please return a book first."
+  end
+  if book.status = "available"
+     book.borrower = user
+     book.status = "checked-out"
+     return
+   puts "The book #{book.title} is now checked out."
+   
 
   end
 
   def check_in(book)
   end
+ 
 end
 
 class Borrower
   def initialize(name)
     @name = name
     puts "Welcome to the Library, #{@name}."
+    @books = []
+    @borrowed_books = []
   end
 
   def borrowed_books
+    @borrowed_books
   end
 
   def name
@@ -59,6 +72,8 @@ class Book
     @title = title
     @author = author
     puts "I love the book #{@title}, by #{@author}!"
+    @status = "available"
+    @borrower = nil
   end
 
   def title
@@ -67,5 +82,21 @@ class Book
 
   def author
     @author
+  end
+
+  def status
+    @status
+  end
+
+  def status=(new_value)
+    @status = new_value
+  end
+
+  def borrower
+    @borrower
+  end
+
+  def borrower=(new_value)
+    @borrower = new_value
   end
 end
