87cc1e78087052be66d93a9189e7e8bfaf9b2c01
Jesse Brandenburg <jesselbrandenburg@gmail.com>
2013-11-15 10:48:42 -0600

    created method to return a book's title, author; created method to add books to the Library's @books array; created method to list each book in the library, but it does not yet list the book's status

--- library.rb
+++ library.rb
@@ -3,22 +3,31 @@ class Library
     @books = []
   end
 
-  def books
-  end
+  # def books
+
+  # end
 
   def list_books
+    @books.each do |book|
+      puts "#{book.title} by #{book.author}"
+    end
   end
 
   def borrowed_books
+
   end
 
   def available_books
+
   end
 
   def add_book(book)
+    @books << book
+    puts "You have added #{book.title} by #{book.author} to the Library!"
   end
 
   def check_out(user, book)
+
   end
 
   def check_in(book)
@@ -27,6 +36,8 @@ end
 
 class Borrower
   def initialize(name)
+    @name = name
+    puts "Welcome to the Library, #{@name}."
   end
 
   def borrowed_books
@@ -36,6 +47,7 @@ class Borrower
   end
 
   def borrowed_books_count
+
   end
 
   def borrowed_books_list
@@ -44,5 +56,16 @@ end
 
 class Book
   def initialize(title, author)
+    @title = title
+    @author = author
+    puts "I love the book #{@title}, by #{@author}!"
+  end
+
+  def title
+    @title
+  end
+
+  def author
+    @author
   end
 end
